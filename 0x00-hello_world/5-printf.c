1b6ab31d9dfded740d0ecf2fccb9dafd89c3f7b9
umarusow <umarujurgorsow@gmail.com>
2022-10-13 15:04:35 -0700

    sow

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#iclude <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (good)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}
