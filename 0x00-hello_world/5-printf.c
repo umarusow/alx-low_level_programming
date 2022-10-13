ecccfc8fbf4a9cb3e92479d15b3a397911161d9c
umarusow <umarujurgorsow@gmail.com>
2022-10-13 11:16:38 -0700

    sow

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - text
+ * Return: Always 0 (good)
+ */
+int main(void)
+{
+	ptintf("with proper grammar, but the outcome is a a piece of art,\n");
+		return (0) 
+
+
+
+}
