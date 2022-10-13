eafc1d96dbd57641b26d0936325d8948aa8bb0f9
umarusow <umarujurgorsow@gmail.com>
2022-10-13 10:19:08 -0700

    sow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #!/bin/bash
 #include <stdio.h>
+
+/**
+ * main - test
+ *
+ * return: Always 0 (good)
+ */
 int main(void)
 {
 	puts("\Programming is like building a multilingual puzzle");
 	return (0);
-
-
-
 
 
 }
