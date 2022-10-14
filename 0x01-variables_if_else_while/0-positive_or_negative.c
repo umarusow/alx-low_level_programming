fea155ebb6a72893e4cba9bdf0e3ccf8f3b9f031
umarusow <umarujurgorsow@gmail.com>
2022-10-14 11:34:23 -0700

    sow

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - block
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negetive\n", n);
+	} else if (n ==0)
+	{
+		printf("%dis zero\n", n);
+	} else 
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
