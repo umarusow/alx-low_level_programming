9738d6abe27176a7fe63612d428854f90340f7b0
umarusow <umarujurgorsow@gmail.com>
2022-10-16 02:38:14 -0700

    sow

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		putchar(letter)
+	}
+	putchar('\n');
+
+	return (0);
+}
+
