7f4979e3736d1908714e4d1ce0a3ef7d7faf0277
umarusow <umarujurgorsow@gmail.com>
2022-10-16 04:35:05 -0700

    sow

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+
 /**
-* main - prints
-* Return: Always 0.
-*/
+ *main - Prints all possible combinat.
+ *Return: Always 0.
+ */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
+	putchar((num % 10) + '0');
+	if (num == 9)
+	continue;
 
-		putchar(',');
-		putchar(' ');
+	putchar(',');
+	putchar(' ');
 	}
 
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
