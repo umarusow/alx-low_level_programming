6c9ad6ada871a0d464f88cb477f43a41a0c646e7
umarusow <umarujurgorsow@gmail.com>
2022-10-18 16:07:52 -0700

    print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - print
+* Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)
+
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
+
