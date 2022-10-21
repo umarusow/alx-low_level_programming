1d1070bd19e216c0b46e84a2e41cb8c924ea01ae
umarusow <umarujurgorsow@gmail.com>
2022-10-21 05:18:38 -0700

    sow

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
 
@@ -33,7 +32,7 @@ if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 		else
 	{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day)
+		printf("Remaining days: %d\n", 365 - day);
 	}
 	}
 }
